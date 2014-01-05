88c4d5eece06fa72e46d5fd8850a27f2cb22cb97
Gleywson <Gleywson@192.168.0.2>
2014-01-05 17:42:03 -0300

    Adição de novos arquivos

---
+++ ag/src/java/modelo/Casal.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Gleywson
+ */
+public class Casal {
+
+    private Cromossomo pai;
+    private Cromossomo mae;
+    private TipoCrossover tipoCrossover;
+
+    public Casal(Cromossomo pai, Cromossomo mae, TipoCrossover crossover) {
+        this.pai = pai;
+        this.mae = mae;
+        this.tipoCrossover = crossover;
+    }
+
+    public Cromossomo getPai() {
+        return pai;
+    }
+
+    public void setPai(Cromossomo pai) {
+        this.pai = pai;
+    }
+
+    public Cromossomo getMae() {
+        return mae;
+    }
+
+    public void setMae(Cromossomo mae) {
+        this.mae = mae;
+    }
+
+    public TipoCrossover getTipoCrossover() {
+        return tipoCrossover;
+    }
+
+    public void setTipoCrossover(TipoCrossover tipoCrossover) {
+        this.tipoCrossover = tipoCrossover;
+    }
+
+    public Cromossomo[] cruzar(float taxaDeMutacao) {
+        return pai.crossover(mae, taxaDeMutacao, tipoCrossover);
+    }
+
+    public Cromossomo[] getConjuges() {
+        Cromossomo[] pais = {pai, mae};
+        return pais;
+    }
+
+}
---
+++ ag/src/java/modelo/Populacao.java
@@ -0,0 +1,291 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Random;
+
+/**
+ *
+ * @author Gleywson
+ */
+public class Populacao {
+
+    private Collection<Cromossomo> individuos;
+    private Collection<Cromossomo> temp;
+    private int tamanhoDaPopulacao;
+    private TipoCrossover tipoCrossover;
+    private Selecao selecao;
+
+    public Populacao(int tamanho, int comprimentoCromossomo) {
+        this.tamanhoDaPopulacao = tamanho;
+        individuos = new ArrayList<Cromossomo>(tamanho);
+        temp = new ArrayList<Cromossomo>(tamanho);
+        initPopulacao(comprimentoCromossomo);
+    }
+
+    public Selecao getSelecao() {
+        return selecao;
+    }
+
+    public void setSelecao(Selecao selecao) {
+        this.selecao = selecao;
+    }
+
+    public Collection<Cromossomo> getIndividuos() {
+        return individuos;
+    }
+
+    public Collection<Cromossomo> getTemp() {
+        return temp;
+    }
+
+    public int getTamanhoDaPopulacao() {
+        return tamanhoDaPopulacao;
+    }
+
+    public TipoCrossover getTipoCrossover() {
+        return tipoCrossover;
+    }
+
+    public void setTipoCrossover(TipoCrossover tipoCrossover) {
+        this.tipoCrossover = tipoCrossover;
+    }
+
+    private void initPopulacao(int tamanho) {
+        for (int i = 1; i <= tamanhoDaPopulacao; i++) {
+
+            individuos.add(new Cromossomo(tamanho));
+        }
+    }
+
+        public void geracao(float taxaDeCruzamento, float taxaDeMutacao) {
+        Random random = new Random();
+        double chances = random.nextFloat();
+
+        //aqui aconttece o elitismo
+        Cromossomo[] elite = new Cromossomo[2];
+        for (int i = 0; i < 2; i++) {
+            elite[i] = elitismo();
+        }
+        temp.addAll(Arrays.asList(elite));
+
+        while (temp.size() < tamanhoDaPopulacao) {
+            Casal casal = casamento();
+            if (chances < taxaDeCruzamento) {
+                Cromossomo[] novosIndividuos = casal.cruzar(taxaDeMutacao);
+                temp.addAll(Arrays.asList(novosIndividuos));
+
+            } else {
+                Cromossomo[] cromossomos = casal.getConjuges();
+                temp.addAll(Arrays.asList(cromossomos));
+            }
+        }
+        individuos.clear();
+        individuos.addAll(temp);
+        temp.clear();
+    }
+
+    private Cromossomo elitismo() {
+        Cromossomo maisApto = null;
+        float valor = Float.MAX_VALUE;
+        Iterator<Cromossomo> iterator = individuos.iterator();
+
+        while (iterator.hasNext()) {
+            Cromossomo elite = iterator.next();
+            if (elite.getFitness() < valor) {
+                valor = elite.getFitness();
+                maisApto = elite;
+            }
+        }
+        return maisApto;
+    }
+
+    private Casal casamento() {
+        Cromossomo pai = seleciona(selecao);
+
+        Cromossomo mae;
+        do {
+            mae = seleciona(selecao);
+        } while (pai.equals(mae));
+
+        return new Casal(pai, mae, tipoCrossover);
+    }
+
+    private Cromossomo torneio(int n) {
+        if (n < 1 && n > individuos.size()) {
+            System.err.println("Numero de competidores fora da faixa válida!");
+        }
+
+        Cromossomo[] temporario = new Cromossomo[individuos.size()];
+        individuos.toArray(temporario);
+
+        Cromossomo[] competidores = new Cromossomo[n];
+        int[] numeroCompetidores = new int[n];
+        Random random = new Random();
+        for (int i = 0; i < numeroCompetidores.length; i++) {
+            boolean repete;
+            do {
+                repete = false;
+                numeroCompetidores[i] = 1 + random.nextInt(individuos.size() - 1);
+                for (int j = 0; j < i; j++) {
+                    if (numeroCompetidores[i] == numeroCompetidores[j]) {
+                        repete = true;
+                        break;
+                    }
+                }
+            } while (repete);
+        }
+
+        for (int i = 0; i < competidores.length; i++) {
+            int current = numeroCompetidores[i];
+            competidores[i] = temporario[current];
+        }
+
+        Cromossomo maisApto = null;
+        float valor = Float.MAX_VALUE;
+        for (Cromossomo competidor : competidores) {
+            if (competidor.getFitness() < valor) {
+                valor = competidor.getFitness();
+                maisApto = competidor;
+            }
+        }
+        return maisApto;
+    }
+
+    private Cromossomo roleta() {
+        double somatorioFitness = 0;
+        double somatorioFitComplement = 0;
+        double acumulado = 0;
+        Cromossomo retorno = null;
+
+        Collection<Object[]> roleta = new ArrayList<>();
+        Iterator<Cromossomo> i = individuos.iterator();
+        Iterator<Cromossomo> it = individuos.iterator();
+
+        while (i.hasNext()) {
+            somatorioFitness += i.next().getFitness();
+        }
+
+        while (it.hasNext()) {
+            somatorioFitComplement += it.next().getFitComplement(somatorioFitness);
+        }
+
+        Iterator<Cromossomo> it2 = individuos.iterator();
+        while (it2.hasNext()) {
+            Cromossomo individuo = it2.next();
+            roleta.add(new Object[]{
+                new Double(acumulado),
+                new Double(acumulado + individuo.getFitComplement(somatorioFitness) / somatorioFitComplement),
+                individuo
+            });
+
+            acumulado += individuo.getFitComplement(somatorioFitness) / somatorioFitComplement;
+        }
+        double sorteio = Math.random();
+
+        Iterator<Object[]> it3 = roleta.iterator();
+        while (it3.hasNext()) {
+            Object[] atual = it3.next();
+
+            double limiteInferior = ((Double) atual[0]).doubleValue();
+            double limiteSuperior = ((Double) atual[1]).doubleValue();
+            Cromossomo individuo = (Cromossomo) atual[2];
+
+            if (sorteio >= limiteInferior && sorteio < limiteSuperior) {
+                retorno = individuo;
+                break;
+            }
+        }
+
+        return retorno;
+    }
+
+    private Cromossomo seleciona(Selecao selecao) {
+        int n = 3;
+        Cromossomo c;
+        switch (selecao) {
+            case ROLETA:
+                c = roleta();
+                break;
+            case TORNEIO:
+                c = torneio(n);
+                break;
+            default:
+                c = torneio(n);
+        }
+        return c;
+    }
+
+    public String saida(int geracao) {
+        String saida;
+        String x1 = "x1 = [";
+        String x2 = "x2 = [";
+
+        Iterator<Cromossomo> it = getIndividuos().iterator();
+        while (it.hasNext()) {
+            float valores[] = it.next().getX();
+            x1 += valores[0] + ",";
+            x2 += valores[1] + ",";
+        }
+        x1 += "];";
+        x2 += "];";
+
+        StringBuilder builder = new StringBuilder(x1);
+        builder.deleteCharAt(x1.length() - 3);
+        x1 = builder.toString();
+
+        StringBuilder builder2 = new StringBuilder(x2);
+        builder2.deleteCharAt(x2.length() - 3);
+        x2 = builder2.toString();
+
+        saida = ""
+                + x1 + "\n"
+                + x2 + "\n"
+                + "plot(x1, x2, '*');\n"
+                + "title('geracao: " + geracao + "');\n"
+                + "xlabel('eixo x1');\n"
+                + "ylabel('eixo x2');\n"
+                + "pause(0.10);";
+
+        return saida;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder saida = new StringBuilder("Populacao: \n");
+
+        Iterator<Cromossomo> iterator = individuos.iterator();
+
+        while (iterator.hasNext()) {
+            saida.append("\n");
+            saida.append(iterator.next().toString());
+        }
+        return saida.toString();
+    }
+
+    public float getPiorIndividuo() {
+        Cromossomo pior = null;
+        float valor = Float.MIN_VALUE;
+        Iterator<Cromossomo> iterator = individuos.iterator();
+
+        while (iterator.hasNext()) {
+            Cromossomo cromossomo = iterator.next();
+            if (cromossomo.getFitness() > valor) {
+                valor = cromossomo.getFitness();
+                pior = cromossomo;
+            }
+        }
+        return pior.getFitness();
+    }
+
+    public float getMelhorIndividuo() {
+        return elitismo().getFitness();
+    }
+}
